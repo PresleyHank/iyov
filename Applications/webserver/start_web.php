eafce150ef0194b938bd34189c4b13ec9ba2673c
nicecp <459850867@qq.com>
2016-11-06 18:22:48 +0800

    index.php

--- Applications/webserver/start_web.php
+++ Applications/webserver/start_web.php
@@ -10,7 +10,8 @@ $web = new WebServer("http://0.0.0.0:8080");
 $web->count = 4;
 
 // Set the root of domains
-$web->addRoot('www.iyov.com', __DIR__);
+$web->addRoot('www.iyov.io', __DIR__);
+$web->addRoot('iyov.io', __DIR__);
 
 // run all workers
 Worker::runAll();
